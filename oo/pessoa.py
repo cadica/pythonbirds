45f81a6edff8c5be6f45c8d7eada7b437d5358b1
cadica <r1c4r!)0>
2020-01-21 11:52:02 -0300

    criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=50):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Ricardo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Arruda'
+    print(p.nome)
+    print(p.idade)
